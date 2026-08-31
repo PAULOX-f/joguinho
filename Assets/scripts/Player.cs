a645e92ea974d629bb8159dfcefbe05dc20a1a49
Paulo Alexandre <paulo.onf@gmail.com>
2026-08-31 20:16:28 -0300

    add pt2

--- Assets/scripts/Player.cs
+++ Assets/scripts/Player.cs
@@ -1,7 +1,16 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
+
+    Rigidbody2D rb;
+    float xDir, yDir;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,5 +21,23 @@ public class Player : MonoBehaviour
     void Update()
     {
         
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        rb.linearVelocityX = xDir + 15;
+        rb.linearVelocityY = yDir + 15;
+    }
+
+    void OnMove(InputValue value)
+    {
+        print(value.Get<Vector2>());
+        xDir = value.Get<Vector2>().x;
+        yDir = value.Get<Vector2>().y;
     }
 }
