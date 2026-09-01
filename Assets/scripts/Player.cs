4ca4cb536eeee025114a6a44435f2288b58a412c
Caio Teodoro Portela <caioteodoro2012@gmail.com>
2026-08-31 21:37:42 -0300

    testes movimento player

--- Assets/scripts/Player.cs
+++ Assets/scripts/Player.cs
@@ -3,36 +3,52 @@ using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] private float speed;
+    
     Rigidbody2D rb;
     float xDir, yDir;
 
-    bool isMoving = false;
+    private Vector2 atualPosition;
+    private Vector2 targetPosition;
+    private bool isMoving = false;
+    private float direcao;
 
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
     }
 
-    void FixedUpdate()
+    void Update()
     {
+        atualPosition = new  Vector2(transform.position.x, transform.position.y);
+        
         if (isMoving)
-        {
             Move();
-        }
     }
 
     void Move()
     {
-            rb.linearVelocityX = xDir + 15;
-            rb.linearVelocityY = yDir + 15;
-        
+        while (atualPosition.x <= targetPosition.x && atualPosition.y <= targetPosition.y && direcao > 0 || atualPosition.x >= targetPosition.x && atualPosition.y >= targetPosition.y && direcao < 0)
+        {
+            rb.linearVelocityX = xDir * speed;
+            rb.linearVelocityY = yDir * speed;
+        }
+        isMoving = false;
     }
 
     void OnMove(InputValue value)
     {
-        isMoving = !isMoving;
+        //isMoving = !isMoving;
         print(value.Get<Vector2>());
         xDir = value.Get<Vector2>().x;
         yDir = value.Get<Vector2>().y;
+        
+        targetPosition = atualPosition + (value.Get<Vector2>() * 1.25f);
+        isMoving = true;
+
+        if (xDir != 0)
+            direcao = xDir;
+        else
+            direcao = yDir;
     }
 }
